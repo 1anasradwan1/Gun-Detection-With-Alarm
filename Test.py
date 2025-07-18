9f44adea5f465d51ff02d3e4f6e30a58f5c2b887
AnasRadwan <anas.radwan.ai@gmail.com>
2025-07-19 02:31:23 +0300

    sec

--- Test.py
+++ Test.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import imutils
 
-gun_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+gun_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 camera = cv2.VideoCapture(0)
 
 first_frame = None
